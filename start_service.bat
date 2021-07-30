4798422c07b46a8415cf9bcfe6156a10c4450c80
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-07-30 19:06:12 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop LaylaRobot
-net start LaylaRobot
+net stop Mizuharasmexybot
+net start MizuharaSmexybot
