eff8050c770280fdd3b6fbd8364754772e0c006c
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-07-31 21:31:18 +0530

    Update musicplayer.py

--- MizuharaSmexyBot/modules/musicplayer.py
+++ MizuharaSmexyBot/modules/musicplayer.py
@@ -20,7 +20,4 @@ __help__ = """
 ❍ /end - stop music play
 ❍ /userbotjoin - invite assistant to your chat
 ❍ /admincache - Refresh admin list
-=>>   *Use*
-1) Add @LaylaRobot in your group to admin with all permission
-2) Add Assistant I'd to your group @LaylaAssistant
 """
