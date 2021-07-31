ea9f54aa3cb85839c3c8f9a81b387872408f4145
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-07-31 21:29:16 +0530

    Update music.py

--- MizuharaSmexyBot/modules/music.py
+++ MizuharaSmexyBot/modules/music.py
@@ -12,9 +12,9 @@ from tswift import Song
 from youtube_dl import YoutubeDL
 from youtubesearchpython import SearchVideos
 
-from LaylaRobot.conf import get_str_key
-from LaylaRobot.pyrogramee.pluginshelper import get_text, progress
-from LaylaRobot import pbot
+from MizuharaSmexyBot.conf import get_str_key
+from MizuharaSmexyBot.pyrogramee.pluginshelper import get_text, progress
+from MizuharaSmexyBot import pbot
 
 GENIUS = get_str_key("GENIUS_API_TOKEN", None)
 
