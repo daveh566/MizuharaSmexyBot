e66c77271e6bd67edfdf9066b9c33c1e6659c389
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-07-31 21:35:26 +0530

    Update nsfw.py

--- MizuharaSmexyBot/modules/nsfw.py
+++ MizuharaSmexyBot/modules/nsfw.py
@@ -4,14 +4,14 @@ import nekos
 import requests
 from PIL import Image
 from telegram import ParseMode
-from LaylaRobot import dispatcher, updater
-import LaylaRobot.modules.sql.nsfw_sql as sql
-from LaylaRobot.modules.log_channel import gloggable
+from MizuharaSmexyBot import dispatcher, updater
+import MizuharaSmexyBot.modules.sql.nsfw_sql as sql
+from MizuharaSmexyBot.modules.log_channel import gloggable
 from telegram import Message, Chat, Update, Bot, MessageEntity
 from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import CommandHandler, run_async, CallbackContext
-from LaylaRobot.modules.helper_funcs.filters import CustomFilters
-from LaylaRobot.modules.helper_funcs.chat_status import user_admin
+from MizuharaSmexyBot.modules.helper_funcs.filters import CustomFilters
+from MizuharaSmexyBot.modules.helper_funcs.chat_status import user_admin
 from telegram.utils.helpers import mention_html, mention_markdown, escape_markdown
 
 @run_async
