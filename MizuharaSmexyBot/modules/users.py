3aa06741752b443ec7ef489cdcd5b9f47474b6fc
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-07-31 22:07:38 +0530

    Update users.py

--- MizuharaSmexyBot/modules/users.py
+++ MizuharaSmexyBot/modules/users.py
@@ -11,10 +11,10 @@ from telegram.ext import (
     run_async,
 )
 
-import LaylaRobot.modules.sql.users_sql as sql
-from LaylaRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from LaylaRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from LaylaRobot.modules.sql.users_sql import get_all_users
+import MizuharaSmexyBot.modules.sql.users_sql as sql
+from MizuharaSmexyBot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from MizuharaSmexyBot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from MizuharaSmexyBot.modules.sql.users_sql import get_all_users
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
