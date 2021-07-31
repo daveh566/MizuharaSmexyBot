11158c15c2030b2cc41cbabcfd92d796af97f7ae
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-07-31 18:49:08 +0530

    Update anime.py

--- MizuharaSmexyBot/modules/anime.py
+++ MizuharaSmexyBot/modules/anime.py
@@ -5,8 +5,8 @@ import textwrap
 import bs4
 import jikanpy
 import requests
-from LaylaRobot import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
+from MizuharaSmexyBot import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
+from MizuharaSmexyBot.modules.disable import DisableAbleCommandHandler
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
                       Update)
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
