568ae78a4fbaadfa2ac5d3a28292baa2b3fbb9f8
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-07-31 19:06:38 +0530

    Update cleaner.py

--- MizuharaSmexyBot/modules/cleaner.py
+++ MizuharaSmexyBot/modules/cleaner.py
@@ -1,15 +1,15 @@
 
 import html
 
-from LaylaRobot import ALLOW_EXCL, CustomCommandHandler, dispatcher
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
-from LaylaRobot.modules.helper_funcs.chat_status import (
+from MizuharaSmexyBot import ALLOW_EXCL, CustomCommandHandler, dispatcher
+from MizuharaSmexyBot.modules.disable import DisableAbleCommandHandler
+from MizuharaSmexyBot.modules.helper_funcs.chat_status import (
     bot_can_delete,
     connection_status,
     dev_plus,
     user_admin,
 )
-from LaylaRobot.modules.sql import cleaner_sql as sql
+from MizuharaSmexyBot.modules.sql import cleaner_sql as sql
 from telegram import ParseMode, Update
 from telegram.ext import (
     CallbackContext,
