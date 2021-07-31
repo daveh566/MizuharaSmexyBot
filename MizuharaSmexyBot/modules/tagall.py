3a46337085f0baa2fc2576fbbbf6d9fc05d3198f
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-07-31 21:59:26 +0530

    Update tagall.py

--- MizuharaSmexyBot/modules/tagall.py
+++ MizuharaSmexyBot/modules/tagall.py
@@ -9,8 +9,8 @@
 
 from pyrogram import filters
 
-from LaylaRobot.pyrogramee.pluginshelper import admins_only, get_text
-from LaylaRobot import pbot
+from MizuharaSmexyBot.pyrogramee.pluginshelper import admins_only, get_text
+from MizuharaSmexyBot import pbot
 
 
 @pbot.on_message(filters.command("tagall") & ~filters.edited & ~filters.bot)
