db14ecd2a26cebf2e1d1285e5cf313fa35b59782
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-07-31 20:46:14 +0530

    Update disasters.py

--- MizuharaSmexyBot/modules/disasters.py
+++ MizuharaSmexyBot/modules/disasters.py
@@ -3,7 +3,7 @@ import json
 import os
 from typing import Optional
 
-from LaylaRobot import (
+from MizuharaSmexyBot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -13,18 +13,18 @@ from LaylaRobot import (
     WOLVES,
     dispatcher,
 )
-from LaylaRobot.modules.helper_funcs.chat_status import (
+from MizuharaSmexyBot.modules.helper_funcs.chat_status import (
     dev_plus,
     sudo_plus,
     whitelist_plus,
 )
-from LaylaRobot.modules.helper_funcs.extraction import extract_user
-from LaylaRobot.modules.log_channel import gloggable
+from MizuharaSmexyBot.modules.helper_funcs.extraction import extract_user
+from MizuharaSmexyBot.modules.log_channel import gloggable
 from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
 
-ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "LaylaRobot/elevated_users.json")
+ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "MizuharaSmexyBot/elevated_users.json")
 
 
 def check_user_id(user_id: int, context: CallbackContext) -> Optional[str]:
