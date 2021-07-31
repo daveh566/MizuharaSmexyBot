85eeb053fbf416c014ba7f726f8ad8a255231285
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-07-31 17:58:29 +0530

    Update __core.py

--- MizuharaSmexyBot/modules/__core.py
+++ MizuharaSmexyBot/modules/__core.py
@@ -1,15 +1,15 @@
-from LaylaRobot import telethn as tbot
-from LaylaRobot.events import register
+from MizuharaSmexyBot import telethn as tbot
+from MizuharaSmexyBot.events import register
 import os
 import asyncio
 import os
 import time
 from datetime import datetime
-from LaylaRobot import OWNER_ID, DEV_USERS
-from LaylaRobot import TEMP_DOWNLOAD_DIRECTORY as path
-from LaylaRobot import TEMP_DOWNLOAD_DIRECTORY
+from MizuharaSmexyBot import OWNER_ID, DEV_USERS
+from MizuharaSmexyBot import TEMP_DOWNLOAD_DIRECTORY as path
+from MizuharaSmexyBot import TEMP_DOWNLOAD_DIRECTORY
 from datetime import datetime
-water = './LaylaRobot/resources/yone.jpg'
+water = './MizuharaSmexyBot/resources/yone.jpg'
 client = tbot
 
 @register(pattern=r"^/send ?(.*)")
@@ -21,7 +21,7 @@ async def Prof(event):
     thumb = water
     message_id = event.message.id
     input_str = event.pattern_match.group(1)
-    the_plugin_file = "./LaylaRobot/modules/{}.py".format(input_str)
+    the_plugin_file = "./MizuharaSmexyBot/modules/{}.py".format(input_str)
     if os.path.exists(the_plugin_file):
      message_id = event.message.id
      await event.client.send_file(
