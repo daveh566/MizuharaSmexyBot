d72c2b2c8b7d42cd87547bab80bd80de983fce30
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-07-31 21:51:49 +0530

    Update shell.py

--- MizuharaSmexyBot/modules/shell.py
+++ MizuharaSmexyBot/modules/shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
-from LaylaRobot import LOGGER, dispatcher
-from LaylaRobot.modules.helper_funcs.chat_status import dev_plus
+from MizuharaSmexyBot import LOGGER, dispatcher
+from MizuharaSmexyBot.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
