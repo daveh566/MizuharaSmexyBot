0215aa4b38e843cadd4652db2700694e187f8837
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-07-31 22:15:31 +0530

    Update zombies.py

--- MizuharaSmexyBot/modules/zombies.py
+++ MizuharaSmexyBot/modules/zombies.py
@@ -6,7 +6,7 @@ from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins
 
-from LaylaRobot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
+from MizuharaSmexyBot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
 
 # =================== CONSTANT ===================
 
