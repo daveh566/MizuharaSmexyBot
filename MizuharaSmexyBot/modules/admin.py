2a8fd6320c2fd922980238adee9ce0056452733a
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-07-31 18:45:05 +0530

    Update admin.py

--- MizuharaSmexyBot/modules/admin.py
+++ MizuharaSmexyBot/modules/admin.py
@@ -5,9 +5,9 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from LaylaRobot import DRAGONS, dispatcher
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
-from LaylaRobot.modules.helper_funcs.chat_status import (
+from MizuharaSmexyBot import DRAGONS, dispatcher
+from MizuharaSmexyBot.modules.disable import DisableAbleCommandHandler
+from MizuharaSmexyBot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_pin,
     can_promote,
@@ -15,19 +15,19 @@ from LaylaRobot.modules.helper_funcs.chat_status import (
     user_admin,
     ADMIN_CACHE,
 )
-from LaylaRobot.helper_extra.admin_rights import (
+from MizuharaSmexyBot.helper_extra.admin_rights import (
     user_can_pin,
     user_can_promote,
     user_can_changeinfo,
 )
 
-from LaylaRobot.modules.helper_funcs.extraction import (
+from MizuharaSmexyBot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from LaylaRobot.modules.log_channel import loggable
-from LaylaRobot.modules.helper_funcs.alternate import send_message
-from LaylaRobot.modules.helper_funcs.alternate import typing_action
+from MizuharaSmexyBot.modules.log_channel import loggable
+from MizuharaSmexyBot.modules.helper_funcs.alternate import send_message
+from MizuharaSmexyBot.modules.helper_funcs.alternate import typing_action
 
 
 @run_async
