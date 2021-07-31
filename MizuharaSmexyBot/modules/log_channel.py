da2fd74a472bb3fd868b5ce816177b08a72d5f57
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-07-31 21:18:14 +0530

    Update log_channel.py

--- MizuharaSmexyBot/modules/log_channel.py
+++ MizuharaSmexyBot/modules/log_channel.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 from telegram.ext import CallbackContext
 
-from LaylaRobot.modules.helper_funcs.misc import is_module_loaded
+from MizuharaSmexyBot.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
@@ -13,9 +13,9 @@ if is_module_loaded(FILENAME):
     from telegram.ext import CommandHandler, JobQueue, run_async
     from telegram.utils.helpers import escape_markdown
 
-    from LaylaRobot import EVENT_LOGS, LOGGER, dispatcher
-    from LaylaRobot.modules.helper_funcs.chat_status import user_admin
-    from LaylaRobot.modules.sql import log_channel_sql as sql
+    from MizuharaSmexyBot import EVENT_LOGS, LOGGER, dispatcher
+    from MizuharaSmexyBot.modules.helper_funcs.chat_status import user_admin
+    from MizuharaSmexyBot.modules.sql import log_channel_sql as sql
 
     def loggable(func):
         @wraps(func)
