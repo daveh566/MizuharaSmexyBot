4289cf080c9d6af5235dfb31449b1e5b3760b146
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-07-31 21:36:53 +0530

    Update paste.py

--- MizuharaSmexyBot/modules/paste.py
+++ MizuharaSmexyBot/modules/paste.py
@@ -1,6 +1,6 @@
 import requests
-from LaylaRobot import dispatcher
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
+from MizuharaSmexyBot import dispatcher
+from MizuharaSmexyBot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
