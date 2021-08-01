ff3def57a96592db99e4b8c84619413554f7ef40
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-08-01 19:59:30 +0530

    Update welcome_sql.py

--- MizuharaSmexyBot/modules/SQL/welcome_sql.py
+++ MizuharaSmexyBot/modules/SQL/welcome_sql.py
@@ -2,8 +2,8 @@ import random
 import threading
 from typing import Union
 
-from LaylaRobot.modules.helper_funcs.msg_types import Types
-from LaylaRobot.modules.sql import BASE, SESSION
+from MizuharaSmexyBot.modules.helper_funcs.msg_types import Types
+from MizuharaSmexyBot.modules.sql import BASE, SESSION
 from sqlalchemy import BigInteger, Boolean, Column, Integer, String, UnicodeText
 
 DEFAULT_WELCOME = "Hey {first}, how are you?"
