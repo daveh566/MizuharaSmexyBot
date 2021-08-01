e5c308a29f97fac5ce016adc5a300f5263c34791
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-08-01 19:47:50 +0530

    Update cust_filters_sql.py

--- MizuharaSmexyBot/modules/SQL/cust_filters_sql.py
+++ MizuharaSmexyBot/modules/SQL/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from LaylaRobot.modules.helper_funcs.msg_types import Types
-from LaylaRobot.modules.sql import BASE, SESSION
+from MizuharaSmexyBot.modules.helper_funcs.msg_types import Types
+from MizuharaSmexyBot.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
