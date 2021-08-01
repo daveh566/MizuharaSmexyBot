bc59ca94557b13b2501a92124aa5ea49757a5ba3
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-08-01 19:39:52 +0530

    Update __init__.py

--- MizuharaSmexyBot/modules/SQL/__init__.py
+++ MizuharaSmexyBot/modules/SQL/__init__.py
@@ -1,4 +1,4 @@
-from LaylaRobot import DB_URI
+from MizuharaSmexyBot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
