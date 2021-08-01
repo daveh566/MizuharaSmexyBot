bcabd50b5e2a5c337b7d52f08be5838b24728c8f
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-08-01 21:20:02 +0530

    Update misc.py

--- MizuharaSmexyBot/modules/helper_funcs/misc.py
+++ MizuharaSmexyBot/modules/helper_funcs/misc.py
@@ -1,7 +1,7 @@
 from math import ceil
 from typing import Dict, List
 
-from LaylaRobot import NO_LOAD
+from MizuharaSmexyBot import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
 
@@ -67,7 +67,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("Back", callback_data="layla_back"),
+                EqInlineKeyboardButton("Back", callback_data="Mizuhara_back"),
              EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
