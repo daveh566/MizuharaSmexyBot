a379422b16ee0a455253ee3ea3cb410d9dc46a8b
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-08-01 19:42:42 +0530

    Update approve_sql.py

--- MizuharaSmexyBot/modules/SQL/approve_sql.py
+++ MizuharaSmexyBot/modules/SQL/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Integer, func, distinct
 
-from LaylaRobot.modules.sql import BASE, SESSION
+from MizuharaSmexyBot.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):
