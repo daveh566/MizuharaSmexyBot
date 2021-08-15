3a7f10cd93ab65bbbb3a01035faefb71bb68fb72
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-08-15 15:01:10 +0530

    Update misc.py

--- MizuharaSmexyBot/modules/helper_funcs/misc.py
+++ MizuharaSmexyBot/modules/helper_funcs/misc.py
@@ -72,7 +72,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
              EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="layla_back")]]
+        pairs += [[EqInlineKeyboardButton("Back", callback_data="Mizuhara_back")]]
 
     return pairs
 
